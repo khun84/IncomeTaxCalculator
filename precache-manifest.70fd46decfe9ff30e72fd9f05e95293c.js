5e973f3176e293f235ed9805bfa09a1148ca6ac6
Adrian Chin <syncing95@gmail.com>
2020-01-24 13:39:25 +0800

    Updates

---
+++ precache-manifest.70fd46decfe9ff30e72fd9f05e95293c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfb94f675346ad9c7e552173fc97341",
+    "url": "/IncomeTaxCalculator/index.html"
+  },
+  {
+    "revision": "f2e272b2d5d9bd92293d",
+    "url": "/IncomeTaxCalculator/static/js/2.284243e7.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/IncomeTaxCalculator/static/js/2.284243e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bfcd68cb9e9e0733c5b",
+    "url": "/IncomeTaxCalculator/static/js/main.f7d2e6da.chunk.js"
+  },
+  {
+    "revision": "50680e4831fd1c223281",
+    "url": "/IncomeTaxCalculator/static/js/runtime-main.a7d37bd1.js"
+  }
+]);
