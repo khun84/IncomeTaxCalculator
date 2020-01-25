a4e251f8598bd003bfda3c41c3935f4a7bb75d59
Adrian Chin <syncing95@gmail.com>
2020-01-26 00:38:14 +0800

    added view summary button

--- src/ParentApp.js
+++ src/ParentApp.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import Header from './Header';
 import QuestionContainer from './QuestionContainer';
+import Button from '@material-ui/core/Button';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
 export default class ParentApp extends Component {
 
@@ -10,6 +12,12 @@ export default class ParentApp extends Component {
             <Header />
             <QuestionContainer />
 
+            <div className="viewSummaryButton">
+            <Button variant="contained" color="primary" size="large" startIcon={<MonetizationOnIcon />}>
+                View Summary
+            </Button>
+            </div>
+                
             </div> 
         );
     }
